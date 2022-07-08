d49b11a9ede782247b0ddedc0b0e411bc439b28f
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-07-08 18:38:31 +0300

    четность числа

---
+++ Les_01/Homework/HW_06/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определение четного числа
+Console.WriteLine("Введите число: ");
+int a =int.Parse(Console.ReadLine());
+if (a % 2==0)
+{
+     Console.WriteLine("Число четное");
+           
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
