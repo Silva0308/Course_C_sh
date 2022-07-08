4bfa7e00174c77e1a4db070685ce1af50edf2ff5
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-07-08 18:49:16 +0300

    перечисление четных чисел

---
+++ Les_01/Homework/HW_08/Program.cs
@@ -0,0 +1,11 @@
+﻿// Четные числа в числе
+Console.WriteLine("Введите число: ");
+int number =int.Parse(Console.ReadLine());
+int i = 2;
+while (i<=number)
+{
+   Console.WriteLine (i);
+   i = i+2;
+}
+if (number <2)
+Console.WriteLine ("Четных чисел в составе нет");
