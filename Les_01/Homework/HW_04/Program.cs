91d1c05b571a3d9162a11e42fa6294bfe03820e2
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-07-08 18:27:49 +0300

    max number

---
+++ Les_01/Homework/HW_04/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Сравнение трех чисел
+Console.WriteLine("Введите первое число:");
+int num_1 =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num_2 =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num_3 =int.Parse(Console.ReadLine());
+int max = 0;
+if (num_1>num_2)
+{
+    max =num_1;
+}
+else 
+{
+    max =num_2;
+}
+if (max< num_3)
+{
+    max = num_3;
+}
+Console.WriteLine ("Наибольшее число:");
+Console.WriteLine (max);
