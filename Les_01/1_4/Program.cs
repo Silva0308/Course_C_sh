bdc836393ba0044472bf3640d53b402ea723971a
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-07-08 17:53:44 +0300

    расстояние между числами

---
+++ Les_01/1_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Расстояние между числами
+Console.WriteLine("Write a number: ");
+int a =int.Parse(Console.ReadLine());
+int i=0;
+if ( a==0 )
+{
+    Console.WriteLine (a);
+}
+else if ( a>0 )
+{
+   i=-a;
+while(i<=a)
+{ 
+Console.WriteLine(i); 
+i++;
+} 
+}
+else if(a<0)
+{
+i=-a;
+while (i>=a)
+{
+Console.WriteLine (i);
+    i--;
+}
+}
