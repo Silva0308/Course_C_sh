cedf48f7ddd909459e269428bea37429604bfd7f
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-07-12 21:06:58 +0300

    task3_var2

---
+++ Les_2/Homework/t03_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число 
+//и удаляет вторую цифру это числа
+
+int SomeNumber = new Random().Next(100,1000);
+Console.WriteLine(SomeNumber);
+int a = SomeNumber/100;
+int c = SomeNumber%10;
+Console.Write (a);
+Console.WriteLine (c);
