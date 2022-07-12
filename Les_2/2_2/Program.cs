2308a5ab44da6e5a551236c8c141a3635a666a3d
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-07-12 10:55:40 +0300

    2_2

---
+++ Les_2/2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа
+//Console.WriteLine("Write a number 10-99:");
+//int a = int.Parse(Console.ReadLine());
+
+
+int MaxDigit (int Number)
+{
+    if (Number / 10 > Number %10)
+    return Number / 10;
+    else
+    return Number %10;
+}
+int a = new Random().Next(10,100);
+Console.WriteLine(a);
+Console.WriteLine(MaxDigit(a));
