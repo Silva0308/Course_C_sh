9185ff3ea08bd4cffd7779d0d431b1f646fb7c4c
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-07-12 22:12:58 +0300

    чуть меняла параметры

--- Les_2/2_3/Program.cs
+++ Les_2/2_3/Program.cs
@@ -4,15 +4,18 @@
 
 //    34, 5 -> не кратно, остаток 4
 //    16, 4 -> кратно
-
+Console.WriteLine ("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
 void Method1 (int a, int b)
 {
     if (a%b == 0)
     Console.WriteLine ("кратно");
     else
     {
-        Console.WriteLine ($"не кратно {a%b}");
+        Console.WriteLine ($"не кратно {b}");
     }
 }
 
-Method1 (16,4);
+Method1 (numberA,numberB);
