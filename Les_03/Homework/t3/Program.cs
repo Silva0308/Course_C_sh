370bcd613569d90091c857929e71fb8107d40673
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-07-15 15:03:45 +0300

    cube

---
+++ Les_03/Homework/t3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+void Square ( int N) 
+{
+    int index = 1;
+    while (index <= N)
+    {
+        Console.WriteLine (Math.Pow(index,3));
+        index ++;
+    }
+}
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+Square (N);
+
+
+
