7457144e994b813bfec1274968c90a0a4e410456
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-07-15 11:08:53 +0300

    task3

---
+++ Les_03/3_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+void Square ( int N) 
+{
+    int index = 1;
+    while (index <= N)
+    {
+        Console.WriteLine (Math.Pow(index,2));
+        index ++;
+    }
+}
+Square (5);
