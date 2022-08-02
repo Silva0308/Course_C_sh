abb2bb697e2788d4435845f16f7061627c615a8e
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-02 11:07:19 +0300

    factorial

---
+++ Les_04/4_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+int Method(int num)
+{
+    int AllSum = 1;
+    for (int i = 1; i <=num; i++)
+        AllSum = AllSum * i;
+    return AllSum;
+}
+Console.WriteLine(Method(4));
