1e8953e8fc76b2f44128a441edc26df3b33c427d
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-02 10:53:52 +0300

    длина числа

---
+++ Les_04/4_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число 
+//и выдаёт количество цифр в числе.
+void NumLenght(int num)
+{
+    string a = num.ToString();
+    Console.WriteLine(a.Length);
+}
+NumLenght(2568);
