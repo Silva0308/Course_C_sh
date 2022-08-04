395e8bfff9f80ea61e95f2b66223bf445da75557
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-04 09:19:38 +0300

    Возведение в натуральную степень

---
+++ Les_04/HW/01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+int NumExp(int a, int b)
+{
+int exp = a;
+    if (b == 1)
+    {
+        return exp;
+    }
+    else
+    {
+        for (int i = 2; i <= b; i++)
+        {
+            exp = exp * a;
+        }
+    }
+return exp;
+}
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine(NumExp(a, b));
