41b175eac03f4cfd7b9a14bde3775667527ada85
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-04 08:54:35 +0300

    массив

---
+++ Les_04/4_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+void Random01Array(int num)
+{
+    int[] array = new int[num];
+    for(int i=0; i < num; i++)
+    {
+        array[i]=new Random().Next(0, 2);
+        Console.Write($"{array[i]} ");
+    }
+}
+Random01Array(8);
