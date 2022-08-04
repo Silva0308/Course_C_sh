f0144ec42ff64d471d8e476d9bb73a5f50519db6
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-04 11:04:59 +0300

    сумма цифр

---
+++ Les_04/HW/02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+int SumNum(int a)
+{
+    int res = 0;
+    while (a > 0)
+    {
+        res = res + a % 10;
+        a = a / 10;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine ($"Сумма цифр в числе: {(SumNum(num))}");
