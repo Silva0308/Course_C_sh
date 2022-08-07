d299d2bea852e774fdf5056f3b1075fc3ed29e9e
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-07 11:17:57 +0300

    нечетные элементы

---
+++ Les_05/HW/02/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+int SumOdd (int [] array)
+{
+int sum = 0;
+for (int i = 0; i < array.Length; i+=2)
+{
+    sum = sum+array[i];
+}
+return sum;
+}
+int[] arr_1 = MassNums(11, -5, 10);
+Print(arr_1);
+Console.WriteLine ($"Сумма нечетных элементов: {SumOdd(arr_1)}");
