4e786e2c8a227bcc54cef11b533762aefda55163
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-08 08:55:51 +0300

    в один цикл

--- Les_05/HW/03/Program.cs
+++ Les_05/HW/03/Program.cs
@@ -25,20 +25,16 @@ double[] MassNums(int size, double from, double to)
 void MinMax(double[] array)
 {
     double min = array[0];
+    double max = array[0];
     for (int i = 1; i < array.Length; i++)
     {
         if (min > array[i])
             min = array[i];
-    }
-    Console.WriteLine("min = {0}", min);
-
-    double max = array[0];
-    for (int i = 1; i < array.Length; i++)
-    {
         if (max < array[i])
             max = array[i];
-    }
+   }
     Console.WriteLine("max = {0}", max);
+    Console.WriteLine("min = {0}", min);
     Console.WriteLine($"Разница между максимальным и минимальным значением массива = {max - min}");
 }
 double[] arr_1 = MassNums(8, -10, 10);
