54c9143d137fdac01e0b9ac1710e314223c22e00
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-09 14:07:00 +0300

    положительные элементы

---
+++ Les_06/HW/02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Количество положительных элементов: {count}");
