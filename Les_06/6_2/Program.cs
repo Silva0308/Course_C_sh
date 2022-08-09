4e815cd2eaa627024a3e342d236857c3c303ff4e
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-09 10:56:11 +0300

    перевод в двоичную

---
+++ Les_06/6_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+string Change (int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = num%2+result;
+        num/=2;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int a =int.Parse(Console.ReadLine());
+Console.WriteLine(Change(a));
