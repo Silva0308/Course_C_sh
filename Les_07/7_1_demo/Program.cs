01a67ed75e5a8872e5a75235ef6fb4f243aa5568
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-15 21:55:34 +0300

    создание двумерного массива

---
+++ Les_07/7_1_demo/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+void Print(int[,] array)
+{
+    int row_size = array.GetLength(0);
+    int column_size = array.GetLength(1);
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int [,] MassNum(int row, int column, int from , int  to)
+{
+    int[,] array = new int [row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(from,to);
+        } 
+    }
+    return array;
+}
+
+Console.Write( "Enter the numbers of rows:");
+int row = int.Parse(Console.ReadLine());
+Console.Write( "Enter the numbers of columns:");
+int column = int.Parse(Console.ReadLine());
+int [,] arr_1 = MassNum (row, column, 1, 11);
+Print (arr_1);
+
