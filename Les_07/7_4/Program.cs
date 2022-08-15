4527c298e5fad236c3a2881832b53692070e537c
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-15 22:54:06 +0300

    search num

---
+++ Les_07/7_4/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте двумерный массив. Введите элемент, и найдите
+// первое его вхождение, выведите позиции по горизонтали и
+// вертикали, или напишите, что такого элемента нет.
+void Print(int[,] array)
+{
+    int row_size = array.GetLength(0);
+    int column_size = array.GetLength(1);
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int [,] MassNum(int row, int column, int from , int  to)
+{
+    int[,] array = new int [row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(from,to);
+        } 
+    }
+    return array;
+}
+
+string PositionNum (int [,] array, int a)
+{
+int row_size = array.GetLength(0);
+    int column_size = array.GetLength(1);
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            if (a == array [i,j])
+            {
+                return $"({i+1}, {j+1})";
+            }
+        }
+    }
+    return "no";
+}
+Console.Write( "Enter the numbers of rows:");
+int row = int.Parse(Console.ReadLine());
+Console.Write( "Enter the numbers of columns:");
+int column = int.Parse(Console.ReadLine());
+int [,] arr_1 = MassNum (row, column, 1, 11);
+
+Console.Write( "Enter number:");
+int num = int.Parse(Console.ReadLine());
+Print (arr_1);
+
+
+Console.WriteLine(PositionNum (arr_1, num));
+
