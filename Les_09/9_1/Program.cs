6cf40f3b2cc12f5d25f0b2afdfbc223edca47cd4
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-19 10:45:51 +0300

    numbers print

---
+++ Les_09/9_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+void Numbers(int num)
+{
+ if (num == 0) return;
+ Numbers (num-1);
+ Console.Write($"{num}, ");
+}
+
+Console.WriteLine ("Please write n");
+int n = int.Parse(Console.ReadLine()); 
+Numbers (n);
