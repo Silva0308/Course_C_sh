b7806dcef551332f76ad1d0b7f2f59f78481de86
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-08-23 20:51:50 +0300

    numbers list

---
+++ Les_09/HW/01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N.
+void ValueSetMinMax(int min, int max)
+{
+    if (max%2 == 0)
+    {
+        if (max<min) return;
+    ValueSetMinMax(min,max-2);
+    Console.Write($"{max}, ");
+    }
+    else
+    {
+        if (max-1<min) return;
+    ValueSetMinMax(min,max-2);
+    Console.Write($"{max-1}, ");
+    }
+    
+}
+
+ValueSetMinMax(3,9);
